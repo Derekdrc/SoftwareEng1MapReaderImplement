8e0db1d0e52af06196ef0ef8b65e85653dc0c53d
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2022-11-16 12:11:06 -0500

    Initial commit

---
+++ MapReader/MapReader/Source.cpp
@@ -0,0 +1,11 @@
+//Scrum Leader: Derek D'Arcy
+//Members: 
+
+//Implementation phase to create a map reader application for the blind
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
