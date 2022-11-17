cb88718c58216b5b5a798eceea138eddf8614715
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2022-11-17 16:23:01 -0500

    Started on using sapi library for text to speech

--- MapReader/MapReader/Source.cpp
+++ MapReader/MapReader/Source.cpp
@@ -5,14 +5,31 @@
 //Implementation phase to create a map reader application for the blind
 
 #include <iostream>
-#include "GPS.h"
+#include <string>
+#include <sapi.h>
+/*#include "GPS.h"
 #include "Map.h"
 #include "Location.h"
 #include "Navi.h"
 #include "Voice.h"
-#include "Camera.h"
+#include "Camera.h"*/
 using namespace std;
 
 int main() {
+	ISpVoice* pVoice = NULL;
+	if (FAILED(::CoInitialize(NULL)))
+		return EXIT_FAILURE;
+
+	HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void**)&pVoice);
+	hr = pVoice->Speak(L"Welcome to the map reader app for the blind", 0, NULL);
+
+	if (SUCCEEDED(hr))
+	{
+		pVoice->Release();
+		pVoice = NULL;
+	}
+	::CoUninitialize();
+
+	return EXIT_SUCCESS;
 	cout << "Welcome to the map reader for the blind";
 }
