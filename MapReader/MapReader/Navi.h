f300b3418e7487853a3d0f4c3af3a368b1573abd
Jadervin <78097308+Jadervin@users.noreply.github.com>
2022-11-24 17:00:10 -0500

    Added statements to functions and inheritance

--- MapReader/MapReader/Navi.h
+++ MapReader/MapReader/Navi.h
@@ -8,18 +8,71 @@
 using namespace std;
 
 
-class Navigation {
+class Navigation: protected Voice, Camera, GPS
+{
 private:
 	Camera frontCam;
 	Camera leftCam;
 	Camera RightCam;
 
 public:
-	void askAboutAction() {
+	void askAboutAction() 
+	{
+		if (confirmPhraseSaid() == true)
+		{
+			//Asks what the user wants
 
+			setPhraseSaid(0);
+
+			readVoiceCommand();
+		}
 	}
 
-	void askAboutWalkInfo() {
+	void askAboutWalkInfo() 
+	{
+		//if the person asks to set the destination
+		{
+			setCurrentLocation();
+
+			setDistance();
+		}
+		if (getWalkDistance() == 0)
+		{
+			//Tell the person they have arrived to their destination
+
+			setDistanceReached(0);
+
+		}
+		if (getOnSidewalk() == true)
+		{
+			//Tell the person about it
+
+			setOnSidewalk(0);
+		}
+		if (getisLight() == true)
+		{
+			//Tell the person about it
+
+			setIsLight(0);
+		}
+		if (getIsSign() == true)
+		{
+			//Tell the person about it
+
+			setIsSign(0);
+		}
+		if (getCarNear() == true)
+		{
+			//Tell the person about it
+
+			setCarNear(0);
+		}
+		if (getDogUsingBathroom() == true)
+		{
+			//Tell the person about it
+
+			setDogUsingBathroom(0);
+		}
 
 	}
 };
